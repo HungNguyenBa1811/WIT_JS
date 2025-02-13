0f10bef26a2bc777afc07f4e64b94b98a6a4ce89
HungNguyenBa1811 <nahh1811@gmail.com>
2025-02-13 11:13:38 +0700

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 const btnAcceptedArray = document.querySelectorAll('button.confirm');
 const btnDeleteArray = document.querySelectorAll('button.delete');
 const btnState = document.querySelector('button.toggle');
+const btnPanel = document.getElementById('btn-expand');
+const modal = document.getElementById('modal');
+const panel = document.getElementById('panel');
 let isExpand = false;
 
-if (btnState) { 
+if (btnState) {
     btnState.addEventListener('click', () => {
         btnState.innerHTML = isExpand ? 'See All' : 'Collapse';
         isExpand = !isExpand;
     });
 }
+
+btnPanel.onclick = () => {
+    if (modal.classList.contains('-z-1')) {
+        // modal
+        modal.classList.remove('-z-1');
+        modal.classList.add('z-20');
+        modal.classList.add('backdrop-brightness-50');
+
+        // panel
+        panel.classList.remove('-translate-x-85');
+    }
+};
+
+modal.onclick = (e) => {
+    if (!panel.contains(e.target)) {
+        // modal
+        modal.classList.remove('backdrop-brightness-50');
+        modal.classList.remove('z-20');
+        modal.classList.add('-z-1');
+
+        // panel
+        panel.classList.add('-translate-x-85');
+    }
+};
 
 btnAcceptedArray.forEach((btn) => {
     btn.addEventListener('click', (e) => {
@@ -23,14 +50,14 @@ btnAcceptedArray.forEach((btn) => {
         let parent = e.target.parentElement.parentElement.parentElement;
         let content = Object.values(e.target.parentElement.parentElement.children);
         console.log(content);
-        content.forEach(el => el.remove());
+        content.forEach((el) => el.remove());
         // faded
         setTimeout(() => {
             parent.style.opacity = 0;
             setTimeout(() => {
                 parent.remove();
             }, 500);
-        }, 1000)
+        }, 1000);
     });
 });
 
